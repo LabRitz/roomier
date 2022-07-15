bc09062695b79e37b0a3ca801a8a85ac8c76e528
Brennan <blee3395@gmail.com>
2022-07-15 19:35:15 -0400

    Implemented App.jsx routing

---
+++ client/components/App.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom';
+
+import Login from './Login.jsx';
+// import Signup from './Signup.jsx';
+// import Home from './Home.jsx';
+// import CreatePost from './CreatePost.jsx';
+// import Profile from './Profile.jsx';
+
+const App = (props) => {
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path='/' component={Login} />
+        {/* <Route path='/signup' component={Signup}/>
+        <Route path='/home' component={Home}/>
+        <Route path='/createPost' component={CreatePost}/>
+        <Route path='/profile' component={Profile}/> */}
+
+        <Redirect to='/' />  
+      </Switch>
+    </Router>
+  )
+
+}
+
+export default App;
---
+++ client/components/Login.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+const Login = (props) => {
+
+  return (
+    <div className='router'>
+      <h2>Find a Roommate</h2>
+
+    </div>
+  )
+
+}
+
+export default Login;
