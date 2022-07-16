eb96c158ffe86b69c9ec8927f8a9a3daad712848
Addy Chandrashekar <addy@Addys-MacBook-Pro-2.local>
2022-07-16 14:40:00 -0400

    finished login

--- client/components/Login.jsx
+++ client/components/Login.jsx
@@ -1,16 +1,26 @@
 import React, { Component } from 'react';
 import {Link} from 'react-router-dom'
 
+
+let retrievedID = '';
+
 const handleLogin = (e) => {
     e.preventDefault();
+
     console.log({username: document.getElementById('username').value, password: document.getElementById('password').value});
     document.getElementById('username').value = '';
     document.getElementById('password').value = '';
-    // fetch('/login', {
-    //     method: 'POST',
-    //     headers: {'Content-Type': 'application/json'},
-    //     body: {username: document.getElementById('username').value, password: document.getElementById('password').value}}
-    // );
+
+    retrievedID = "brennan"
+
+  //   fetch('/login', {
+  //     method: 'POST',
+  //     headers: {'Content-Type': 'application/json'},
+  //     body: {username: document.getElementById('username').value, password: document.getElementById('password').value}}
+  // ).then(data => {
+  //   const {_id} = data[0];
+  //   retrievedID = _id;
+  // })
 }
 
 const Login = (props) => {
@@ -20,6 +30,9 @@ const Login = (props) => {
       <input type={'email'} id="username" placeholder='Enter your email address'></input>
       <input type={'password'} id="password" placeholder='Enter your password'></input>
       <button type='submit' id='submit' onClick={handleLogin}>Login</button>
+      <div className='homeLink'>
+        <Link to='/home' retrievedID = {retrievedID}>Home</Link>
+      </div>
       <Link to='/signup'>Sign up</Link>
       <a href='/login/auth/google'>Log in with google</a>
     </div>
