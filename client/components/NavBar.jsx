b7db280dd86d3c2ffb76fe35765bfc2eb8bf9a00
Brennan <blee3395@gmail.com>
2022-07-18 12:23:33 -0400

    UserData props passed down to container feed for application. Home feed accepts updated GET request other posts

--- client/components/NavBar.jsx
+++ client/components/NavBar.jsx
@@ -26,6 +26,14 @@ const NavBar = (props) => {
     }, 5000)
   }, [phrase])
 
+  function handleSignout() {
+    fetch('/signout')
+      .then(res => res.json())
+      .then(data => {
+        console.log('Signout clicked: ', data)
+      })
+  }
+
   return (
     <div className='nav'>
       <div class="leftBtn">
@@ -71,6 +79,14 @@ const NavBar = (props) => {
             <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" class="bi bi-person-lines-fill" viewBox="0 0 16 16">
               <path d="M6 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm-5 6s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zM11 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5zm.5 2.5a.5.5 0 0 0 0 1h4a.5.5 0 0 0 0-1h-4zm2 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2zm0 3a.5.5 0 0 0 0 1h2a.5.5 0 0 0 0-1h-2z"/>
             </svg>
+          </button>
+        </Link>
+        < Link to={{
+          pathname: '/',
+          state: userData
+          }}>
+          <button class='textBtn version profile' onClick={() => handleSignout()}>
+            Signout
           </button>
         </Link>
       </div>
