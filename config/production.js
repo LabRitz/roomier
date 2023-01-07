2127f950a1c02fda9ee33b6d26959fba1d324273
Brennan <blee3395@gmail.com>
2023-01-07 12:31:12 -0500

    feat: add health route for AWS EBS

--- config/production.js
+++ config/production.js
@@ -1,6 +1,7 @@
 module.exports = {
   DOMAIN: process.env.DOMAIN,
   PORT: process.env.PORT,
+  PROTOCOL: 'https://',
   SERVER_PORT: process.env.SERVER_PORT,
   DATABASE: {
     URI: process.env.ATLAS_URI,
