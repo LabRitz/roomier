2b35b96fe5e32458be13ff20f329bec9dc75364a
Brennan <blee3395@gmail.com>
2023-01-12 10:27:42 -0500

    feat: add loading indicator to Home

--- client/components/Home.jsx
+++ client/components/Home.jsx
@@ -1,12 +1,40 @@
 import React, { Component, useState, useEffect } from "react";
 import { render } from "react-dom";
+import CircularProgress from '@mui/material/CircularProgress';
+import Box from '@mui/material/Box';
 
 import HomeFeed from "./HomeFeed.jsx";
 import { useJsApiLoader, GoogleMap, Marker } from "@react-google-maps/api";
 import Geocode from "react-geocode";
 
+const loadingStyle = {
+  position: 'fixed',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: '60%',
+  display: 'flex',
+  p: 1,
+};
+
+const mapContainerStyle = {
+  width: "35%",
+  height: "90%",
+  bottom: "2%",
+  top: "8%",
+  left: "2%",
+  position: "absolute",
+  borderRadius: "12px",
+  boxShadow: "2px 2px 8px gray",
+};
+
+const GoogleMapsAPIKey = "AIzaSyAdo3_P6D0eBnk6Xj6fmQ4b1pO-HHvEfOM";
+Geocode.setApiKey(GoogleMapsAPIKey);
+
 const Home = ({ userInfo }) => {
   const userData = userInfo;
+
+  const [isLoading, setIsLoading] = useState(false)
 
   const [posts, setPosts] = useState([]);
 
@@ -14,58 +42,48 @@ const Home = ({ userInfo }) => {
   const [zipCode, setZipCode] = useState(userData.zipCode);
   const [distance, setDistance] = useState(1609.344);
 
-  //INSERT OWN GOOGLE MAPS API
-  const GoogleMapsAPIKey = "AIzaSyAdo3_P6D0eBnk6Xj6fmQ4b1pO-HHvEfOM";
-
   const { isLoaded, loadError } = useJsApiLoader({
     googleMapsApiKey: GoogleMapsAPIKey,
   });
 
-  Geocode.setApiKey(GoogleMapsAPIKey);
-
   const [markers, setMarkers] = useState([]);
 
-  function getMarkers() {
+  const getMarkers = () => {
     const tempMarkers = [];
-    for (let i = 0; i < posts.length; i++) {
-      if (posts[i].geoData) {
+    posts.map(post => {
+      if (post.geoData) {
         const posObj = {
-          lng: posts[i].geoData.coordinates[0],
-          lat: posts[i].geoData.coordinates[1],
+          lng: post.geoData.coordinates[0],
+          lat: post.geoData.coordinates[1],
         };
         tempMarkers.push(<Marker position={posObj}></Marker>);
-      } else console.log("no geodata");
-    }
+      } else console.log("ALERT: Post contains no geospatial data");
+    })
     setMarkers(tempMarkers);
   }
 
-  async function getMap() {
+  const getMap = async () => {
     if (isLoaded) {
-      let geocode = await Geocode.fromAddress(zipCode);
-      const { lng, lat } = geocode.results[0].geometry.location;
-      render(
-        <GoogleMap
-          center={{ lat: lat, lng: lng }}
-          zoom={13}
-          mapContainerStyle={{
-            width: "35%",
-            height: "90%",
-            bottom: "2%",
-            top: "8%",
-            left: "2%",
-            position: "absolute",
-            borderRadius: "12px",
-            boxShadow: "2px 2px 8px gray",
-          }}
-        >
-          {markers}
-        </GoogleMap>,
-        document.getElementById("googleMapDiv")
-      );
+      try {
+        const geocode = await Geocode.fromAddress(zipCode);
+        const { lng, lat } = geocode.results[0].geometry.location;
+        render(
+          <GoogleMap
+            center={{ lat: lat, lng: lng }}
+            zoom={13}
+            mapContainerStyle={mapContainerStyle}
+          >
+            {markers}
+          </GoogleMap>,
+          document.getElementById("googleMapDiv")
+        );
+      } catch (err) {
+        console.log('ERROR: Cannot load Google Maps', err)
+      }
     }
   }
 
-  async function getPosts() {
+  const getPosts = async () => {
     let geocode = await Geocode.fromAddress(zipCode);
 
     const reqBody = {
@@ -83,20 +101,17 @@ const Home = ({ userInfo }) => {
     const postsArr = await res.json();
     const newPost = Object.assign(postsArr, { userData: userData });
     setPosts(newPost);
-    return (
-      <>
-        <div className="home">
-          <HomeFeed posts={posts} />
-          <div id="googleMapDiv"></div>
-        </div>
-      </>
-    );
   }
 
-  // Cascading dependency
+  // Cascading dependency of useEffect
   // 1. Get Posts based on ZipCode and Distance
   // 2. Configure markers based on posts
   // 3. Render map based on markers and posts
+  useEffect(() => {
+    // Start Loading indicator
+    setIsLoading(true)
+  },[])
+  
   useEffect(() => {
     getPosts();
   }, [zipCode, distance]);
@@ -107,10 +122,15 @@ const Home = ({ userInfo }) => {
 
   useEffect(() => {
     getMap();
+    setIsLoading(false);
   }, [markers]);
 
   return (
-    <>
+    (isLoading) ? (
+      <Box sx={loadingStyle}>
+        <CircularProgress />
+      </Box>
+    ) : (
       <div className="home">
         <div className="background">
           <img src="https://i.redd.it/za30ryykl7n81.jpg"></img>
@@ -125,10 +145,9 @@ const Home = ({ userInfo }) => {
           distance={distance}
           setDistance={setDistance}
         />
-
         <div id="googleMapDiv"></div>
       </div>
-    </>
+    )
   );
 };
 
