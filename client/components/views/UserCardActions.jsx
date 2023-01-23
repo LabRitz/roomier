89b127993d19371093fe644170fe6073e251627f
Brennan <blee3395@gmail.com>
2023-01-22 21:55:32 -0500

    feat: disable button when user has already applied

--- client/components/views/UserCardActions.jsx
+++ client/components/views/UserCardActions.jsx
@@ -3,11 +3,13 @@ import { Button } from '@mui/material';
 import Tooltip from '@mui/material/Tooltip';
 import Typography from '@mui/material/Typography';
 
-const UserCardActions = ({ application, handleApply }) => {
+const UserCardActions = ({ application, handleApply, hasApplied }) => {
   return (
     <>
       <Tooltip title="Submit contact info">
-        <Button mr={1} onClick={() => handleApply()} size="small">Apply</Button>
+        <Button mr={1} onClick={() => handleApply()} size="small" disabled={hasApplied}>
+          {hasApplied ? 'Applied' : 'Apply'}
+        </Button>
       </Tooltip>
       <Typography pb={0.5} variant="body2" noWrap={true} color="text.secondary">
         {application.length} in review
