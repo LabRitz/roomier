20dea0cf2366087f7ec5154eab1a87fb6bf9bb18
Brennan <blee3395@gmail.com>
2023-01-25 15:40:21 -0500

    feat: add autocomplete location

--- client/components/EditCard.jsx
+++ client/components/EditCard.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import PlacesAutocomplete, { geocodeByAddress, getLatLng } from 'react-places-autocomplete';
 
@@ -32,14 +32,14 @@ const EditCard = ({ postInfo }) => {
     images,
   } = postInfo;
 
-  const [inputStreet1, setInputStreet1] = useState(address.street1)
-  const [inputStreet2, setInputStreet2] = useState(address.street2)
-  const [inputCity, setInputCity] = useState(address.city)
-  const [inputState, setInputState] = useState(address.state)
-  const [inputZipCode, setInputZipCode] = useState(address.zipCode)
-
+  const [location, setLocation] = useState(address)
   const [index, setIndex] = useState(0) // Index for gallery image
  
+  // Need to update the form based on change in post choice
+  useEffect(() => {
+    setLocation(address)
+  }, [postInfo])
+
   const handleClick = (dir) => {
     if (index + dir < 0) setIndex(images.length - 1)
     else if (index + dir > images.length - 1) setIndex(0)
@@ -54,148 +54,279 @@ const EditCard = ({ postInfo }) => {
     // Need logic to remove picture from image array
   }
 
-
-  const handleAddressChange = (address) => {
-    // setAddress({ address });
+  const handleStreet1Change = (address) => {
+    const newAddress = {...location}
+    newAddress.street1 = address
+    setLocation(newAddress)
   };
 
-  const handleSelect = (address) => {
-    geocodeByAddress(address)
-      .then((results) => getLatLng(results[0]))
-      .then((latLng) => console.log('Success', latLng))
-      .catch((error) => console.error('Error', error));
+  const handleStreet1 = async (address) => {
+    const newAddress = {...location}
+    try {
+      const results = await geocodeByAddress(address)
+      let i = 0
+      while (i < results[0].address_components.length) {
+        const res = results[0].address_components[i]
+        if (res.types.indexOf('street_number') != -1) {
+          newAddress.street1 = `${res.short_name} ${results[0].address_components[i+1].short_name}`
+          newAddress.street2= ''
+          i++
+        }
+        else if (res.types.indexOf('locality') != -1) newAddress.city = res.short_name
+        else if (res.types.indexOf('administrative_area_level_1') != -1) newAddress.state = res.short_name
+        else if (res.types.indexOf('postal_code') != -1) newAddress.zipCode = res.short_name
+        i++
+      }
+      setLocation(newAddress)
+    } catch(error) {
+      console.error('Error selecting street', error);
+    }
+  };
+
+  const handleCityChange = (city) => {
+    const newAddress = {...location}
+    newAddress.city = city
+    setLocation(newAddress)
+  };
+
+  const handleCity = async (address) => {
+    const newAddress = {...location}
+    try {
+      const results = await geocodeByAddress(address)
+      let i = 0
+      while (i < results[0].address_components.length) {
+        const res = results[0].address_components[i]
+        if (res.types.indexOf('locality') != -1) newAddress.city = res.short_name
+        else if (res.types.indexOf('administrative_area_level_1') != -1) newAddress.state = res.short_name
+        i++
+      }
+      newAddress.zipCode = ''
+      setLocation(newAddress)
+    } catch(error) {
+      console.error('Error selecting city', error);
+    }
+  };
+
+  const handleStateChange = (state) => {
+    const newAddress = {...location}
+    newAddress.state = state
+    setLocation(newAddress)
+  };
+
+  const handleState = async (address) => {
+    const newAddress = {...location}
+    try {
+      const results = await geocodeByAddress(address)
+      let i = 0
+      while (i < results[0].address_components.length) {
+        const res = results[0].address_components[i]
+        if (res.types.indexOf('administrative_area_level_1') != -1) newAddress.state = res.short_name
+        i++
+      }
+      newAddress.zipCode = ''
+      setLocation(newAddress)
+    } catch(error) {
+      console.error('Error selecting city', error);
+    }
   };
 
 
   return (
-    <>
-      <script
-        src={`https://maps.googleapis.com/maps/api/js?key=${GoogleMapsAPIKey}&libraries=places`}
-        async
-        defer />
-      <PlacesAutocomplete
-        value={address}
-        onChange={handleAddressChange}
-        onSelect={handleSelect}
-        >
-        {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
-          <div>
-            <input
-              {...getInputProps({
-                placeholder: 'Search Places ...',
-                className: 'location-search-input',
-              })}
-              />
-            <div className="autocomplete-dropdown-container">
-              {loading && <div>Loading...</div>}
-              {suggestions.map((suggestion) => {
-                const className = suggestion.active
-                ? 'suggestion-item--active'
-                : 'suggestion-item';
-                // inline style for demonstration purpose
-                const style = suggestion.active
-                ? { backgroundColor: '#fafafa', cursor: 'pointer' }
-                : { backgroundColor: '#ffffff', cursor: 'pointer' };
-                return (
-                  <div
-                  {...getSuggestionItemProps(suggestion, {
-                    className,
-                    style,
+    <div style={{display: 'flex', flexDirection:'row', minWidth:'300px', height:'100%'}}>
+      <Paper elevation={0} sx={{display:'flex', flexDirection:'column', justifyContent:'center', width:'50%'}}>
+        <CardMedia
+          component="img"
+          height="300"
+          image={(!images[index]) ? defaultImg : Object.keys(images[index])[0]}
+        />
+        <CardActions sx={{display: 'flex', justifyContent:'space-evenly'}}>
+          <IconButton color="inherit" onClick={() => handleClick(-1)}>
+            <ArrowBackIosNewIcon fontSize='medium'/>
+          </IconButton>
+          <Button onClick={(e) => handleUpload(e)} size="small">Upload Image</Button>
+          <Button onClick={(e) => handleRemove(e)} size="small">Remove Image</Button>
+          <IconButton color="inherit" onClick={() => handleClick(1)}>
+            <ArrowForwardIosIcon fontSize='medium'/>
+          </IconButton>
+        </CardActions>
+      </Paper>
+      <Paper elevation={0} sx={{p:3, pt:2, pb:1, width:'50%'}}>
+        <FormControl sx={{ display: 'grid', gridTemplateColumns:'2fr 1fr', columnGap:'8px', m: 1 }} size="small">
+          <PlacesAutocomplete
+            value={location.street1}
+            onChange={handleStreet1Change}
+            onSelect={handleStreet1}
+            searchOptions={{
+              componentRestrictions: { country: ['us'] }
+            }}
+            >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <TextField
+                  required
+                  label="Street Address"
+                  value={address.street1}
+                  sx={{ mr:1, width: '100%' }}
+                  size="small"
+                  inputProps={{
+                    ...getInputProps({
+                      placeholder: 'Search Places ...',
+                      className: 'location-search-input',
+                    }),
+                    style: {fontSize: 14}
+                  }} />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                    const style = suggestion.active ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div {...getSuggestionItemProps(suggestion, { className, style })} >
+                        <span style={{fontSize:'12px', overflowX:'hidden'}}>{suggestion.description}</span>
+                      </div>
+                    );
                   })}
-                  >
-                    <span>{suggestion.description}</span>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        )}
-      </PlacesAutocomplete>
-    </>
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+          <TextField
+            label="Apt, Suite, Unit, etc."
+            value={location.street2}
+            inputProps={{ style: {fontSize: 14} }}
+            size="small"
+          />
+        </FormControl>
 
+        <FormControl sx={{ display: 'flex', flexDirection: 'row', m: 1 }} size="small">
+          <PlacesAutocomplete
+            value={location.city}
+            onChange={handleCityChange}
+            onSelect={handleCity}
+            searchOptions={{
+              componentRestrictions: { country: ['us'] },
+              types: ['locality']
+            }}
+            >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <TextField
+                  required
+                  label="City"
+                  value={location.city}
+                  sx={{ mr:1 }}
+                  size="small"
+                  inputProps={{
+                    ...getInputProps({
+                      placeholder: 'Search Places ...',
+                      className: 'location-search-input',
+                    }),
+                    style: {fontSize: 14}
+                  }} />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                    const style = suggestion.active ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div {...getSuggestionItemProps(suggestion, { className, style })} >
+                        <span style={{fontSize:'12px', overflowX:'hidden'}}>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
 
-
-
-
-    // <div style={{display: 'flex', flexDirection:'row', minWidth:'300px', height:'100%'}}>
-    //   <Paper elevation={0} sx={{display:'flex', flexDirection:'column', justifyContent:'center', width:'50%'}}>
-    //     <CardMedia
-    //       component="img"
-    //       height="300"
-    //       image={(!images[index]) ? defaultImg : Object.keys(images[index])[0]}
-    //     />
-    //     <CardActions sx={{display: 'flex', justifyContent:'space-evenly'}}>
-    //       <IconButton color="inherit" onClick={() => handleClick(-1)}>
-    //         <ArrowBackIosNewIcon fontSize='medium'/>
-    //       </IconButton>
-    //       <Button onClick={(e) => handleUpload(e)} size="small">Upload Image</Button>
-    //       <Button onClick={(e) => handleRemove(e)} size="small">Remove Image</Button>
-    //       <IconButton color="inherit" onClick={() => handleClick(1)}>
-    //         <ArrowForwardIosIcon fontSize='medium'/>
-    //       </IconButton>
-    //     </CardActions>
-    //   </Paper>
-    //   <Paper elevation={0} sx={{p:3, pt:2, pb:1, width:'50%'}}>
-    //     <TextField
-    //       label="Street address"
-    //       defaultValue={address.street1}
-    //       size="small"
-    //     />
-    //     <TextField
-    //       label="Apt, etc"
-    //       defaultValue={address.street2}
-    //       size="small"
-    //     />
-    //     <TextField
-    //       label="City"
-    //       defaultValue={address.city}
-    //       size="small"
-    //     />
-    //     <TextField
-    //       label="State"
-    //       defaultValue={address.state}
-    //       size="small"
-    //     />
-    //     <TextField
-    //       label="Zip code"
-    //       defaultValue={address.zipCode}
-    //       size="small"
-    //     />
+          <PlacesAutocomplete
+            value={location.state}
+            onChange={handleStateChange}
+            onSelect={handleState}
+            searchOptions={{
+              componentRestrictions: { country: ['us'] },
+              types: ['administrative_area_level_1']
+            }}
+            >
+            {({ getInputProps, suggestions, getSuggestionItemProps, loading }) => (
+              <div>
+                <TextField
+                  required
+                  label="State"
+                  value={location.state}
+                  sx={{ mr:1, width:60 }}
+                  size="small"
+                  inputProps={{
+                    ...getInputProps({
+                      placeholder: 'Search Places ...',
+                      className: 'location-search-input',
+                    }),
+                    style: {fontSize: 14}
+                  }} />
+                <div className="autocomplete-dropdown-container">
+                  {loading && <div>Loading...</div>}
+                  {suggestions.map((suggestion) => {
+                    const className = suggestion.active ? 'suggestion-item--active' : 'suggestion-item';
+                    const style = suggestion.active ? { backgroundColor: '#fafafa', cursor: 'pointer' }
+                      : { backgroundColor: '#ffffff', cursor: 'pointer' };
+                    return (
+                      <div {...getSuggestionItemProps(suggestion, { className, style })} >
+                        <span style={{fontSize:'12px', overflowX:'hidden'}}>{suggestion.description}</span>
+                      </div>
+                    );
+                  })}
+                </div>
+              </div>
+            )}
+          </PlacesAutocomplete>
+          <TextField
+            required
+            label="Zip code"
+            value={location.zipCode}
+            sx={{ width:100 }}
+            inputProps={{style: {fontSize: 14}}}
+            size="small"
+          />
+        </FormControl>
       
-    //     <FormControl fullWidth sx={{ m: 1 }}>
-    //       <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
-    //       <OutlinedInput
-    //         id="outlined-adornment-amount"
-    //         startAdornment={<InputAdornment position="start">$</InputAdornment>}
-    //         defaultValue={rent}
-    //         label="Amount"
-    //       />
-    //     </FormControl>
+        <FormControl sx={{ m: 1, width:200 }} size="small">
+          <InputLabel htmlFor="outlined-adornment-amount">Amount</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-amount"
+            startAdornment={<InputAdornment sx={{ fontSize:12 }} position="start">$</InputAdornment>}
+            defaultValue={rent}
+            label="Amount"
+            inputProps={{style: {fontSize: 14}}}
+            />
+        </FormControl>
         
-    //     <Typography variant="h6" noWrap={true} color="text.secondary">
-    //       {description.BR}BR | {description.BA}BA | {description.sqFt} sqft
-    //     </Typography>
-    //     <Typography gutterBottom variant="subtitle2" noWrap={true} color="text.primary">
-    //       Available: {new Date(moveInDate).toLocaleDateString()}
-    //     </Typography>
-    //     <Typography gutterBottom variant="body2" noWrap={true} color="text.primary">
-    //       Looking for: {roommate.gender}
-    //     </Typography>
-    //     <TextField
-    //       label="Description"
-    //       defaultValue={bio}
-    //       size="small"
-    //       placeholder="Placeholder"
-    //       multiline
-    //       fullWidth
-    //       sx={{ 
-    //         fontSize: 8, 
-    //         overflowY:'scroll',
-    //         height: '35%'
-    //       }}
-    //     />
-    //   </Paper>
-    // </div>
+        <Typography variant="h6" noWrap={true} color="text.secondary">
+          {description.BR}BR | {description.BA}BA | {description.sqFt} sqft
+        </Typography>
+        <Typography gutterBottom variant="subtitle2" noWrap={true} color="text.primary">
+          Available: {new Date(moveInDate).toLocaleDateString()}
+        </Typography>
+        <Typography gutterBottom variant="body2" noWrap={true} color="text.primary">
+          Looking for: {roommate.gender}
+        </Typography>
+        <TextField
+          label="Description"
+          defaultValue={bio}
+          size="small"
+          placeholder="Placeholder"
+          multiline
+          fullWidth
+          inputProps={{style: {fontSize: 10}}}
+          sx={{ 
+            fontSize: 8, 
+            overflowY:'scroll',
+            height: '35%'
+          }}
+        />
+      </Paper>
+    </div>
   )
 }
 
