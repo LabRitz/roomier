34e84673cedb8867ee9bb7e0096812f0180b262d
Brennan <blee3395@gmail.com>
2023-01-29 17:21:31 -0500

    feat: webpack analyzer

--- webpack/production.webpack.js
+++ webpack/production.webpack.js
@@ -1,7 +1,11 @@
 const commonConfig = require('./common.webpack')
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 const production = {
-  mode: 'production'
+  mode: 'production',
+  plugins: [
+    new BundleAnalyzerPlugin()
+  ]
 };
 
 module.exports = { ...commonConfig, ...production };
