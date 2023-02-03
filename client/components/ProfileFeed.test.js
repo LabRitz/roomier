c34148272d23d7efd2d9444890e1aca978b56288
Brennan <blee3395@gmail.com>
2023-02-03 10:16:52 -0500

    test: profileFeed coverage

---
+++ client/components/ProfileFeed.test.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import ProfileFeed from "./ProfileFeed.jsx";
+
+jest.mock('./ContainerApplication.jsx', () => {
+  return () => <div data-testid="ContainerApplication" /> 
+});
+
+const posts = [{
+  address: {
+    street1: 'Street 1',
+    street2: 'Street 2',
+    city: 'City',
+    state: 'State',
+    zipCode: '12345'
+  },
+  roommate: {gender: 'male'},
+  description: {
+    BR: 1,
+    BA: 1,
+    sqFt: 100,
+    parking: true,
+    smoking: true,
+    pets: true
+  },
+  moveInDate: Date.now(),
+  rent: 100,
+  bio: 'Description',
+  images: {
+    imgUrl: '',
+    imgPath: ''
+  },
+  currUser: {
+    firstName: 'John',
+    lastName: 'Smith',
+    username: 'test@gmail.com'
+  }
+}]
+
+describe("ProfileFeed.jsx", () => {
+
+  it("Renders ProfileFeed component", async () => {    
+    render(<ProfileFeed posts={posts}/>);
+
+    expect(await screen.getByTestId('ContainerApplication')).toBeTruthy();
+  });
+ 
+});
