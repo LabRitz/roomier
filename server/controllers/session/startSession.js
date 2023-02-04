b774ec7c63e023049f61febf2eb18c5145282f81
Brennan <blee3395@gmail.com>
2023-02-04 09:34:21 -0500

    test: startSession coverage

--- server/controllers/session/startSession.js
+++ server/controllers/session/startSession.js
@@ -1,20 +1,19 @@
 const Session = require('../../db/sessionModel');
 
 const startSession = async (req, res, next) => {
-  if (res.locals.user !== null) {
+  if (res.locals.user) {
     try {
       const id = res.locals.user._id.toString();
       await Session.create({ cookieId: id });
       return next(); 
     } catch (err) {
       return next({
-        log : `Error occur in sessionController.startSession ${err}`,
+        log: `ERROR: startSession, ${err}`,
         status: 500,
         message: { err: 'an error occured while attempting to start a session'}
       })
     }
   }
-
   else return next();
 };
 
