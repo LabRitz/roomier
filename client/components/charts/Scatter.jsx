a8cdd63b7d6ec1d534bd1a53a1afb7d58386036e
Brennan <blee3395@gmail.com>
2023-02-08 12:07:05 -0500

    feat: add scales to scatter plot

--- client/components/charts/Scatter.jsx
+++ client/components/charts/Scatter.jsx
@@ -2,13 +2,15 @@ import React, { useMemo, useState, useCallback, useRef } from 'react';
 import { Group } from '@visx/group';
 import { Circle } from '@visx/shape';
 import { RadialGradient } from '@visx/gradient';
-import { scaleLinear } from '@visx/scale';
-import genRandomNormalPoints from '@visx/mock-data/lib/generators/genRandomNormalPoints';
 import { withTooltip, Tooltip } from '@visx/tooltip';
 import { voronoi, VoronoiPolygon } from '@visx/voronoi';
 import { localPoint } from '@visx/event';
+import { Axis, Orientation, SharedAxisProps, AxisScale } from '@visx/axis';
+import { AnimatedAxis, AnimatedGridRows, AnimatedGridColumns } from '@visx/react-spring';
+import { scaleLinear, ScaleInput, coerceNumber } from '@visx/scale';
 
-// const points = genRandomNormalPoints(600, /* seed= */ 0.5).filter((_, i) => i < 600);
+import AreaClosed from '@visx/shape/lib/shapes/AreaClosed';
+import { curveMonotoneX } from '@visx/curve';
 
 const x = (d) => {
   return d.description.sqFt;
@@ -17,12 +19,39 @@ const y = (d) => {
   return d.rent;
 }
 
+const backgroundColor = '#da7cff';
+const axisColor = '#fff';
+const tickLabelColor = '#fff';
+const labelColor = '#340098';
+const gridColor = '#6e0fca';
+
+const tickLabelProps = () =>
+  ({
+    fill: tickLabelColor,
+    fontSize: 12,
+    fontFamily: 'sans-serif',
+    textAnchor: 'middle',
+  });
+
+const getMinMax = (vals) => {
+  const numericVals = vals.map(coerceNumber);
+  return [Math.min(...numericVals), Math.max(...numericVals)];
+};
+
+const margin = {
+  top: 0,
+  right: 0,
+  bottom: 0,
+  left: 0,
+};
+
 let tooltipTimeout;
 
 export default withTooltip(
   ({ posts, width, height, showControls = false, hideTooltip, showTooltip, tooltipOpen, tooltipData, tooltipLeft, tooltipTop }) => {        
     if (width < 10) return null;
     const [showVoronoi, setShowVoronoi] = useState(showControls);
+    const [animationTrajectory, setAnimationTrajectory] = useState('min');
     const svgRef = useRef(null);
 
     const maxPrice = useMemo(() => {
@@ -43,6 +72,27 @@ export default withTooltip(
       range: [height-20, 20],
       clamp: true,
     })
+
+    const AxisComponent = AnimatedAxis
+    const GridRowsComponent = AnimatedGridRows
+    const GridColumnsComponent = AnimatedGridColumns
+
+    const axes = useMemo(() => {
+      const linearValues = [0, 2, 4, 6, 8, 10]; 
+      return [{
+        scale: scaleLinear({
+          domain: getMinMax(linearValues),
+          range: [0, width],
+        }),
+        values: linearValues,
+        tickFormat: (v, index, ticks) =>
+          index === 0 ? 'first' : index === ticks[ticks.length - 1].index ? 'last' : `${v}`,
+        label: 'linear',
+      }];
+    }, [width]);
+
+    const scalePadding = 0;
+    const scaleHeight = height / axes.length - scalePadding;
 
     const voronoiLayout = useMemo(() =>
       voronoi({
@@ -121,6 +171,68 @@ export default withTooltip(
                   />
                 ))}
           </Group>
+
+          <g transform={`translate(${margin.left},${margin.top})`}>
+            {axes.map(({ values, label, tickFormat }, i) => (
+              <g key={`scale-${i}`} transform={`translate(0, ${i * (scaleHeight + scalePadding)})`}>
+                <GridRowsComponent
+                  // force remount when this changes to see the animation difference
+                  key={`gridrows-${animationTrajectory}`}
+                  scale={yScale}
+                  stroke={gridColor}
+                  width={width}
+                  numTicks={15}
+                  animationTrajectory={animationTrajectory}
+                />
+                <GridColumnsComponent
+                  // force remount when this changes to see the animation difference
+                  key={`gridcolumns-${animationTrajectory}`}
+                  scale={xScale}
+                  stroke={gridColor}
+                  height={scaleHeight}
+                  numTicks={10}
+                  animationTrajectory={animationTrajectory}
+                />
+                <AreaClosed
+                  data={values.map((x) => [
+                    (xScale(x) ?? 0),
+                    yScale(10),
+                  ])}
+                  yScale={yScale}
+                  curve={curveMonotoneX}
+                  fill={gridColor}
+                  fillOpacity={0.2}
+                />
+                <AxisComponent
+                  // force remount when this changes to see the animation difference
+                  key={`axis-${animationTrajectory}`}
+                  orientation={Orientation.bottom}
+                  top={scaleHeight}
+                  scale={xScale}
+                  tickFormat={tickFormat}
+                  stroke={axisColor}
+                  tickStroke={axisColor}
+                  tickLabelProps={tickLabelProps}
+                  tickValues={values}
+                  numTicks={undefined}
+                  label={label}
+                  labelProps={{
+                    x: width + 30,
+                    y: -10,
+                    fill: labelColor,
+                    fontSize: 18,
+                    strokeWidth: 0,
+                    stroke: '#fff',
+                    paintOrder: 'stroke',
+                    fontFamily: 'sans-serif',
+                    textAnchor: 'start',
+                  }}
+                  animationTrajectory={animationTrajectory}
+                />
+              </g>
+            ))}
+          </g>
+
         </svg>
         {tooltipOpen && tooltipData && tooltipLeft != null && tooltipTop != null && (
           <Tooltip left={tooltipLeft - 100} top={tooltipTop + 10}>
